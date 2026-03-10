1ff40eafb2d8958b274d35201e71619ebfb5826e
Ck0195 <chaitanyakumar0195@gmail.com>
2026-03-11 00:58:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -184,7 +184,7 @@ async def main():
     await client.start()
     logger.info("Shreya userbot connected 💕")
 
-   @client.on(events.NewMessage(incoming=True))
+    @client.on(events.NewMessage(incoming=True))
     async def handle_message(event):
         user_text = event.raw_text
         try:
